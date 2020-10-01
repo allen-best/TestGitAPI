355b2eea11cf5804e4cf4c729689e4915a5777ba
Allen <ajbest91@gmail.com>
2020-09-30 20:33:58 -0400

    fix: dictionary attribute error

--- test_api_run.py
+++ test_api_run.py
@@ -18,16 +18,16 @@ class TestApiRun(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def testStatusCodeUser(self): 
-        self.assertEqual(printRepos("allen-best").statusCodeUser,200)
+        self.assertEqual(printRepos("allen-best")["statusCodeUser"],200)
     
     def testStatusCodeRepo(self): 
-        self.assertEqual(printRepos("allen-best").statusCodeRepo,200)
+        self.assertEqual(printRepos("allen-best")["statusCodeRepo"],200)
 
     def testRepoCountLength(self): 
-        self.assertEqual(printRepos("allen-best").repoCount,2)
+        self.assertEqual(printRepos("allen-best")["repoCount"],2)
 
     def testCheckFirstRepo(self): 
-        self.assertEqual(printRepos("allen-best").stats,{"Repo":"allen-best.github.io", "Commits": 20})
+        self.assertEqual(printRepos("allen-best")["stats"],{"Repo":"allen-best.github.io", "Commits": 20})
     
 
 if __name__ == '__main__':
