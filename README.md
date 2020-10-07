d2cf898058d57ac69fb6f989a2e04e8fd5eec77d
Allen <ajbest91@gmail.com>
2020-10-07 19:25:27 -0400

    update for mocking

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![build status of master](https://travis-ci.org/allen-best/TestGitAPI.svg?branch=master)](https://travis-ci.org/allen-best/TestGitAPI)
+[![build status of HW05a_Mocking](https://travis-ci.org/allen-best/TestGitAPI.svg?branch=HW05a_Mocking)](https://travis-ci.org/allen-best/TestGitAPI)
 
 # TestGitAPI
 Testing on git api calls. If the testing is failing then we have reached the max api limit for now.
--- test_api_run.py
+++ test_api_run.py
@@ -8,6 +8,7 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 """
 
 import unittest
+from unittest.mock import patch, Mock
 
 from api_run import printRepos
 
@@ -16,15 +17,39 @@ from api_run import printRepos
 
 class TestApiRun(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
+    successful_response = {
+        "id": "allen-best",
+        "statusCodeUser": 200,
+        "repoCount": 2,
+        "stats": [
+            {
+                "Repo":"allen-best", 
+                "Commits": 2
+            },
+            {
+                "Repo":"allen-best", 
+                "Commits": 20
+            }
+        ]
+    }
 
     def testStatusCodeUser(self): 
+        with patch('api_run.printRepos') as mock_get:
+            mock_get.return_value = Mock()
+            mock_get.return_value.json.return_value = self.successful_response
         self.assertEqual(printRepos("allen-best")["statusCodeUser"],200)
 
     def testRepoCountLength(self): 
+        with patch('api_run.printRepos') as mock_get:
+            mock_get.return_value = Mock()
+            mock_get.return_value.json.return_value = self.successful_response
         self.assertEqual(printRepos("allen-best")["repoCount"],2)
 
     def testCheckFirstRepo(self): 
-        self.assertEqual(printRepos("allen-best")["stats"][0],{"Repo":"allen-best.github.io", "Commits": 20})
+        with patch('api_run.printRepos') as mock_get:
+            mock_get.return_value = Mock()
+            mock_get.return_value.json.return_value = self.successful_response
+        self.assertEqual(printRepos("allen-best")["stats"][1],{"Repo":"allen-best.github.io", "Commits": 20})
     
 
 if __name__ == '__main__':
